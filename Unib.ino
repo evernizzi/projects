7e8b9e911533150423126e9c99e5679604ecab66
evernizzi <54252434+evernizzi@users.noreply.github.com>
2019-11-11 21:17:26 -0300

    Update Unib.ino

--- Unib.ino
+++ Unib.ino
@@ -1,24 +1,24 @@
-/*DECLARAÇÃO DE VARIAVEIS*/
+/*DECLARAÇÃO DE VARIAVEIS MOTORES PWM */
 #define MotorA_sentido1 2
 #define MotorA_sentido2 4
 #define MotorB_sentido1 6
 #define MotorB_sentido2 7
 #define MotorA_PWM 3  
 #define MotorB_PWM 5
- 
+
+// * VELOCIDADES 0 - 255 - \\ 0 - PARADO, 1 - DEVAGAR, 2 - NORMAL e 3 - RÁPIDO * //
 #define veloc0 0
 #define veloc1 15
 #define veloc2 35
 #define veloc3 60
  
+// * SENSORES DIREITA - ESQUERDA * //
 #define Sensor_direita 10
 #define Sensor_esquerda 9
- 
 bool direita, esquerda;
  
 void setup() {
   delay(5000);
-  Serial.begin(9600);
   pinMode(MotorA_sentido1, OUTPUT);
   pinMode(MotorA_sentido2, OUTPUT);
   pinMode(MotorB_sentido1, OUTPUT);
@@ -26,13 +26,13 @@ void setup() {
   pinMode(MotorA_PWM, OUTPUT);
   pinMode(MotorB_PWM, OUTPUT);
   pinMode(Sensor_direita, INPUT);
-  pinMode(Sensor_esquerda, INPUT);
+  pinMode(Sensor_esquerda, INPUT);   
  
 }
  
 void loop() {
    //Define o sentido de rotação dos motores
-  digitalWrite(MotorA_sentido1, HIGH);
+  digitalWrite(MotorA_sentido1, HIGH); //
   digitalWrite(MotorA_sentido2, LOW);
   digitalWrite(MotorB_sentido1, LOW);
   digitalWrite(MotorB_sentido2, HIGH);
